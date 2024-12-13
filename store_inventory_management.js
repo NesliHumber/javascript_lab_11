fb1fccc01268c15829c3d1dd29506b626644a9ca
NesliUsta <neslihan_iu81@hotmail.com>
2024-12-13 18:55:56 -0500

    Adds ProductProperties Class and its constructor

---
+++ store_inventory_management.js
@@ -0,0 +1,10 @@
+// Part 1: Setting Up Classes
+
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+}

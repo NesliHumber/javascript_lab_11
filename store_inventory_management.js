98a8f292b0634d11e304475eff9f1cd32fcac587
NesliUsta <neslihan_iu81@hotmail.com>
2024-12-13 18:59:24 -0500

    Adds PerishableProductProperties class

--- store_inventory_management.js
+++ store_inventory_management.js
@@ -1,5 +1,4 @@
 // Part 1: Setting Up Classes
-
 class ProductProperties {
     constructor(name, price, quantity) {
         this.name = name;
@@ -12,4 +11,13 @@ class ProductProperties {
     toString() {
         return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
     }
+}
+
+// Part 2: Adding Inheritance
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
 }

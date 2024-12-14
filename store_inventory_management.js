56487e77f6a21981b21a89a6389648cacc0b1162
NesliUsta <neslihan_iu81@hotmail.com>
2024-12-13 19:04:37 -0500

    Creates PerishableProductProperties instence

--- store_inventory_management.js
+++ store_inventory_management.js
@@ -32,3 +32,6 @@ class PerishableProductProperties extends ProductProperties {
     }
 
 }
+
+// Create perishable product products:
+const milk = new PerishableProductProperties('Milk', 1.50, 10, '2024-12-31');
